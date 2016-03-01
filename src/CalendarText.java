1888b3f92370dd02b4b101590ca335358cf41450
Nata01 <xx932500@gmail.com>
2016-03-02 01:45:09 +0200

    Added new classes

---
+++ src/CalendarText.java
@@ -0,0 +1,44 @@
+import java.io.PrintStream;
+import java.time.DayOfWeek;
+import java.util.Arrays;
+import java.util.List;
+
+public class CalendarText extends Calendar {
+    protected PrintStream printStream;
+
+    public CalendarText(PrintStream printStream) {
+        this.printStream = printStream;
+    }
+
+    protected void printDay(int j) {
+        printStream.printf("%3s ", j);
+    }
+
+    protected void printOffsettedDays(DayOfWeek firstDayOfMonth) {
+        for (int i = 0; i < firstDayOfMonth.getValue(); i++) {
+            printStream.print("    ");
+        }
+    }
+
+    protected void printWeekend(int j) {
+        printStream.printf("%3s ", j);
+    }
+
+    protected void printHighlightedDay(int j) {
+        printStream.printf("%3s ", j);
+    }
+
+    protected void printEndOfWeek() {
+        printStream.println();
+    }
+
+    protected void printHeaderOfMonth() {
+        List<String> dayList = Arrays.asList("Sun", "Mon", "Tue", "Wen", "Thu", "Fri", "Sat");
+        printStartOfWeek();
+        for (String day : dayList) {
+            printStream.printf("%3s ", day);
+        }
+        printEndOfWeek();
+    }
+
+}
--- src/Program.java
+++ src/Program.java
@@ -1,9 +1,14 @@
+import java.io.FileNotFoundException;
+import java.io.PrintStream;
 import java.time.LocalDate;
 import java.time.YearMonth;
 
 public class Program {
-    public static void main(String[] args) {
-        Calendar calendar = new Calendar();
+    public static void main(String[] args) throws FileNotFoundException {
+//        Calendar calendar = new CalendarConsole(System.out);
+//        Calendar calendar = new CalendarText(new PrintStream("calendar.txt"));
+        Calendar calendar = new CalendarHtml(new PrintStream("calendar.html"));
+
 
         int currentDay = LocalDate.now().getDayOfMonth();
         YearMonth currentMonth = YearMonth.now();
